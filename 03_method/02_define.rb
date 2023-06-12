8aec998185cc17760b083b764c0f271c9e7676f1
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-06-12 09:19:55 +0900

    do 03_method

--- 03_method/02_define.rb
+++ 03_method/02_define.rb
@@ -1,6 +1,11 @@
 # Q1.
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
+class A1
+  define_method '//' do
+    '//'
+  end
+end
 
 # Q2.
 # 次の動作をする A2 class を実装する
@@ -11,8 +16,48 @@
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
+class A2
+  # 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
+  def dev_team
+    "SmartHR Dev Team"
+  end
+
+  # 2. initializeに渡した配列に含まれる値に対して、"hoge_" をprefixを付与したメソッドが存在すること
+  def initialize(array)
+    array.each do |name|
+      define_singleton_method("hoge_#{name}") do |arg=nil|
+        # 引数がnilの場合は、dev_teamメソッドを呼ぶこと
+        if arg.nil?
+          dev_team
+        # 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
+        else
+          "hoge_#{name}" * arg
+        end
+      end
+    end
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+module OriginalAccessor
+  def self.included(base)
+    base.define_singleton_method(:my_attr_accessor) do |attr|
+      base.define_method attr do
+        @my_attr_accessor&.fetch(attr) { nil }
+      end
+
+      base.define_method "#{attr}=" do |value|
+        (@my_attr_accessor ||= {})[attr] = value
+
+        if value.is_a?(TrueClass) || value.is_a?(FalseClass)
+          define_singleton_method "#{attr}?" do
+            !!value
+          end
+        end
+      end
+    end
+  end
+end
--- 03_method/03_try_over3_3.rb
+++ 03_method/03_try_over3_3.rb
@@ -5,7 +5,19 @@ TryOver3 = Module.new
 # - `test_` から始まるインスタンスメソッドが実行された場合、このクラスは `run_test` メソッドを実行する
 # - `test_` メソッドがこのクラスに実装されていなくても `test_` から始まるメッセージに応答することができる
 # - TryOver3::A1 には `test_` から始まるインスタンスメソッドが定義されていない
+class TryOver3::A1
+  def run_test
+    nil
+  end
 
+  def method_missing(name, *args)
+    if name.to_s.start_with?("test_")
+      run_test
+    else
+      super
+    end
+  end
+end
 
 # Q2
 # 以下要件を満たす TryOver3::A2Proxy クラスを作成してください。
@@ -15,6 +27,24 @@ class TryOver3::A2
   def initialize(name, value)
     instance_variable_set("@#{name}", value)
     self.class.attr_accessor name.to_sym unless respond_to? name.to_sym
+  end
+end
+
+class TryOver3::A2Proxy
+  def initialize(source)
+    @source = source
+  end
+
+  def method_missing(name, *args)
+    if @source.respond_to?(name)
+      @source.public_send(name, *args)
+    else
+      super
+    end
+  end
+
+  def respond_to?(name, include_private = false)
+    @source.respond_to?(name, include_private)
   end
 end
 
@@ -37,6 +67,8 @@ module TryOver3::OriginalAccessor2
           self.class.define_method "#{name}?" do
             @attr == true
           end
+        elsif respond_to?("#{name}?")
+          self.class.remove_method "#{name}?"
         end
         @attr = value
       end
@@ -51,7 +83,21 @@ end
 # TryOver3::A4::Hoge.run
 # # => "run Hoge"
 # このとき、TryOver3::A4::Hogeという定数は定義されません。
+class TryOver3::A4
+  def self.runners=(consts)
+    @consts = consts
+  end
 
+  def self.const_missing(const)
+    if @consts.include?(const)
+      obj = Object.new
+      obj.define_singleton_method(:run) { "run #{const}" }
+      obj
+    else
+      super
+    end
+  end
+end
 
 # Q5. チャレンジ問題！ 挑戦する方はテストの skip を外して挑戦してみてください。
 #
@@ -59,16 +105,23 @@ end
 module TryOver3::TaskHelper
   def self.included(klass)
     klass.define_singleton_method :task do |name, &task_block|
-      new_klass = Class.new do
-        define_singleton_method :run do
-          puts "start #{Time.now}"
-          block_return = task_block.call
-          puts "finish #{Time.now}"
-          block_return
-        end
+      define_singleton_method name do
+        puts "start #{Time.now}"
+        block_return = task_block.call
+        puts "finish #{Time.now}"
+        block_return
       end
-      new_klass_name = name.to_s.split("_").map{ |w| w[0] = w[0].upcase; w }.join
-      const_set(new_klass_name, new_klass)
+
+      define_singleton_method(:const_missing) do |const|
+        super(const) unless klass.respond_to?(const.downcase)
+
+        obj = Object.new
+        obj.define_singleton_method :run do
+          warn "Warning: TryOver3::A5Task::#{const}.run is deprecated"
+          klass.send name
+        end
+        obj
+      end
     end
   end
 end
