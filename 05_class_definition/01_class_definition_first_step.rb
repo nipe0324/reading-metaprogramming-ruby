394653fd1ccff92b880ad14c0b17b846a2702dbf
Shojiro Yanagisawa <nipe0324@gmail.com>
2023-06-14 01:43:03 +0900

    do 01_class_definition_first_step.rb

--- 05_class_definition/01_class_definition_first_step.rb
+++ 05_class_definition/01_class_definition_first_step.rb
@@ -8,11 +8,16 @@ end
 e1 = ExClass.new
 e2 = ExClass.new
 
+def e2.hello
+end
+
 Judgement.call(e1, e2)
 
 # 2. ExClassを継承したクラスを作成してください。ただし、そのクラスは定数がない無名のクラスだとします。
 #    その無名クラスをそのままJudgement2.call の引数として渡してください(Judgement2.callはテスト側で定義するので実装は不要です)
 
+no_name_class = Class.new(ExClass)
+Judgement2.call(no_name_class)
 
 # 3. 下のMetaClassに対し、次のように`meta_`というプレフィックスが属性名に自動でつき、ゲッターの戻り値の文字列にも'meta 'が自動でつく
 #    attr_accessorのようなメソッドであるmeta_attr_accessorを作ってください。セッターに文字列以外の引数がくることは考えないとします。
@@ -28,6 +33,19 @@ Judgement.call(e1, e2)
 #    meta.meta_hello #=> 'meta world'
 
 class MetaClass
+  class << self
+    def meta_attr_accessor(*names)
+      names.each do |name|
+        define_method("meta_#{name}=") do |value|
+          instance_variable_set('@meta_name', value)
+        end
+
+        define_method("meta_#{name}") do
+          'meta ' + instance_variable_get('@meta_name')
+        end
+      end
+    end
+  end
 end
 
 # 4. 次のようなExConfigクラスを作成してください。ただし、グローバル変数、クラス変数は使わないものとします。
@@ -41,6 +59,17 @@ end
 
 
 class ExConfig
+  class << self
+    attr_accessor :config
+  end
+
+  def config=(value)
+    self.class.instance_variable_set(:@config, value)
+  end
+
+  def config
+    self.class.instance_variable_get(:@config)
+  end
 end
 
 # 5.
@@ -49,7 +78,16 @@ end
 # ただしExOver#hello, ExOver#before, ExOver#afterの実装はそれぞれテスト側で定義しているので実装不要(変更不可)です。
 #
 
+module Hook
+  def hello
+    before
+    super
+    after
+  end
+end
+
 class ExOver
+  prepend Hook
 end
 
 # 6. 次の toplevellocal ローカル変数の中身を返す MyGreeting#say を実装してみてください。
@@ -60,3 +98,9 @@ class MyGreeting
 end
 
 toplevellocal = 'hi'
+
+MyGreeting.class_eval do
+  define_method :say do
+    toplevellocal
+  end
+end
